43f57caecb2ac9ecc301f5220e742599ce3f8c51
YIFAN <fan.n@icloud.com>
2015-04-10 22:49:31 +0800

    Add the all the game file

---
+++ core/src/com/maDe/MyGdxGame.java
@@ -0,0 +1,43 @@
+package com.maDe;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+
+    private SpriteBatch batch;
+    private BitmapFont font;
+
+	
+	@Override
+	public void create () {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.BLUE);
+        font.scale(5);
+	}
+
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+    @Override
+	public void render () {
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        font.draw(batch,"Hello world",100,400);
+
+        batch.end();
+
+	}
+}
